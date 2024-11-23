a10393c746264450b6656100e8b82b5c570023ee
Phanu Sawet <phanu@Phanus-MacBook-Air.local>
2024-11-23 21:02:16 +0700

    Phanu edition not finish

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res) => {
+    res.render('index');
+})
+
+app.listen(PORT, () => {
+    console.log("Application is running on", PORT);
+})
